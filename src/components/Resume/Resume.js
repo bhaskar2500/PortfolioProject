2302d848f2b94ec42c96f73cb88c7c258ce8bd35
Bhaskar Kaushal <bhaskark847@IN.nam.ad.pwcinternal.com>
2020-05-05 17:27:21 +0530

    Text file locally rather from server for Resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Panel } from 'primereact/panel';
 import { Editor } from 'primereact/editor';
 import { getResumeData } from '../../apis/ResumeService';
+import text  from './text.js'; // Relative path to your File
+
 class Resume extends React.Component {
     constructor() {
         super();
@@ -10,11 +12,12 @@ class Resume extends React.Component {
         }
     }
     componentDidMount() {
-        getResumeData().then((data) => {
-            data.json().then((json) => {
-                this.setState({ resumeHtmlValue: json.data });
-            })
-        })
+        // getResumeData().then((data) => {
+        //     data.json().then((json) => {
+        //         this.setState({ resumeHtmlValue: json.data });
+        //     })
+        // })
+        this.setState({resumeHtmlValue:text});
         // storeResumeData()
         // onTextChange={(e) => this.resumeTextChange()}
     }
